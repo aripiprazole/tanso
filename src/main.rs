249b4f1abb857c2413afe16057296a42c867eae3
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2022-12-04 23:31:19 -0300

    feat: add abstraction to send packets

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 #![feature(async_fn_in_trait)]
 #![feature(box_syntax)]
 
-use tokio::io::{AsyncRead, AsyncReadExt};
+use std::time::{SystemTime, UNIX_EPOCH};
+use tokio::io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
+
+trait PacketEncoder {
+    fn packet_id() -> i32;
+
+    async fn encode<W: AsyncWrite + Unpin>(&self, stream: &mut W) -> std::io::Result<()>;
+}
 
 trait PacketDecoder {
     async fn decode<R: AsyncRead + Unpin>(id: i32, stream: &mut R) -> std::io::Result<Box<Self>>;
@@ -32,6 +39,48 @@ impl PacketDecoder for Handshake {
     }
 }
 
+#[derive(Debug)]
+struct Ping {
+    payload: u64,
+}
+
+impl PacketEncoder for Ping {
+    fn packet_id() -> i32 { 0 }
+
+    async fn encode<W: AsyncWrite + Unpin>(&self, stream: &mut W) -> std::io::Result<()> {
+        stream.write_u64(self.payload).await?;
+
+        Ok(())
+    }
+}
+
+impl PacketDecoder for Ping {
+    async fn decode<R: AsyncRead + Unpin>(_id: i32, _stream: &mut R) -> std::io::Result<Box<Self>> {
+        let start = SystemTime::now();
+        let now = start
+            .duration_since(UNIX_EPOCH)
+            .expect("Time went backwards");
+
+        let payload = now.as_secs();
+
+        Ok(box Ping { payload })
+    }
+}
+
+async fn write_var_int<W: AsyncWrite + Unpin>(stream: &mut W, mut value: i32) -> std::io::Result<()> {
+    loop {
+        if value & 0xfffff80 == 0 {
+            stream.write_u8(value as u8).await?;
+            break;
+        }
+
+        stream.write_u8((value & 0x7f | 0x80) as u8).await?;
+        value >>= 7;
+    }
+
+    Ok(())
+}
+
 async fn read_var_int<R: AsyncRead + Unpin>(stream: &mut R) -> std::io::Result<i32> {
     let mut offset = 0;
     let mut value = 0;
@@ -59,14 +108,25 @@ async fn read_string<R: AsyncRead + Unpin>(stream: &mut R) -> std::io::Result<St
 }
 
 async fn read_packet<P: PacketDecoder>(stream: &mut TcpStream) -> std::io::Result<P> {
-    let mut stream = stream;
-
     let _packet_length = read_var_int(stream).await?;
     let packet_id = read_var_int(stream).await?;
 
-    let packet = P::decode(packet_id, &mut stream).await?;
+    let packet = P::decode(packet_id, stream).await?;
 
     Ok(*packet)
+}
+
+async fn write_packet<P: PacketEncoder>(stream: &mut TcpStream, packet: &P) -> std::io::Result<()> {
+    let mut buf: Vec<u8> = vec![];
+
+    write_var_int(&mut buf, P::packet_id()).await?;
+    packet.encode(&mut buf).await?;
+
+    write_var_int(stream, buf.len() as i32).await?;
+
+    stream.write(&mut buf).await?;
+
+    Ok(())
 }
 
 async fn process_status(mut stream: TcpStream) -> std::io::Result<()> {
